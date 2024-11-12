5cc769c4d66ffefc407ef5b78df9549c088917de
Nico Parra <no.gnzlz@gmail.com>
2024-11-12 01:41:57 -0300

    usar el header en todas las rutas

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,10 +2,24 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Edit from "./pages/Edit";
+import { Header } from "./components/Header";
+import Title from "antd/es/typography/Title";
+import { Flex } from "antd";
 
 function App() {
   return (
     <Router>
+      <Header
+        logo={
+          <Flex justify="center" align="center">
+            <img
+              style={{ height: "76px", marginRight: "8px" }}
+              src="logo.png"
+            />
+            <Title style={{ margin: "10px" }}>Lui Admin</Title>
+          </Flex>
+        }
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/edit/:id" element={<Edit />} />
--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -1,10 +1,8 @@
-import { Flex, Typography } from "antd";
+import { Flex } from "antd";
 import { useEffect, useState } from "react";
-import { Header } from "../components/Header";
 import { MenusTable } from "../components/MenusTable";
 
 function Home() {
-  const { Title } = Typography;
   const [menus, setMenus] = useState([]);
 
   const initData = async () => {
@@ -17,17 +15,6 @@ function Home() {
 
   return (
     <>
-      <Header
-        logo={
-          <Flex justify="center" align="center">
-            <img
-              style={{ height: "76px", marginRight: "8px" }}
-              src="logo.png"
-            />
-            <Title style={{ margin: "10px" }}>Lui Admin</Title>
-          </Flex>
-        }
-      />
       <Flex justify="center" style={{ width: "100%" }}>
         <MenusTable data={menus} />
       </Flex>
