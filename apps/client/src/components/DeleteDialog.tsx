0d671734c199dd6f8a7b7a8e5e9e86f0c02ef9dd
Nico Parra <no.gnzlz@gmail.com>
2024-11-22 16:50:36 -0300

    improve delete modal

--- apps/client/src/components/DeleteDialog.tsx
+++ apps/client/src/components/DeleteDialog.tsx
@@ -1,17 +1,52 @@
-import { DeleteOutlined, ExclamationCircleFilled } from "@ant-design/icons";
-import { Button, Modal, Tooltip } from "antd";
+import { CloseOutlined, DeleteFilled, DeleteOutlined } from "@ant-design/icons";
+import { Button, Modal, Tooltip, Image, Flex, Typography } from "antd";
+import { MenuType } from "../interfaces";
 
-const DeleteDialog = ({ onDelete }: { onDelete: () => void }) => {
+const DeleteDialog = ({
+  onDelete,
+  menu,
+}: {
+  onDelete: () => void;
+  menu: MenuType;
+}) => {
   const { confirm } = Modal;
 
   const handleClick = () => {
     confirm({
-      title: "Estas seguro que quieres borrar el menu ?",
-      icon: <ExclamationCircleFilled />,
+      title: `¿Estas seguro que quieres eliminar este menu?`,
+      icon: <></>,
+
+      content: (
+        <Flex style={{ margin: "8px 0" }} align="top">
+          <Image width={160} src={menu.imageUrl} preview={false} />
+          <Flex vertical style={{ marginLeft: 16 }}>
+            <Typography.Text style={{ fontWeight: 500 }}>
+              {menu.name}
+            </Typography.Text>
+            <Typography.Text>{`$${menu.price.toLocaleString("es-AR")}`}</Typography.Text>
+          </Flex>
+        </Flex>
+      ),
 
       onOk() {
         onDelete();
       },
+      okText: (
+        <Flex gap={4}>
+          <DeleteOutlined />
+          Eliminar
+        </Flex>
+      ),
+      okType: "danger",
+      okButtonProps: {
+        type: "primary",
+      },
+      cancelText: (
+        <Flex gap={4}>
+          <CloseOutlined />
+          Cancelar
+        </Flex>
+      ),
     });
   };
 
--- apps/client/src/components/MenusTable.tsx
+++ apps/client/src/components/MenusTable.tsx
@@ -30,61 +30,62 @@ const handleDelete = async (id: number) => {
   });
 };
 
-const columns: TableColumnsType<MenuType> = [
-  {
-    title: "Imagen",
-    dataIndex: "imageUrl",
-    render: (imageUrl: string) => <StyledImage width={100} src={imageUrl} />,
-  },
-  {
-    title: "Nombre",
-    dataIndex: "name",
-  },
-  {
-    title: "Precio",
-    dataIndex: "price",
-    render: (price: number) => `$${price.toLocaleString("es-AR")}`,
-  },
-  {
-    align: "center",
-    title: "Activo",
-    dataIndex: "isDaysMenu",
-    render: (isDaysMenu: boolean) => (
-      <p>
-        {isDaysMenu ? (
-          <CheckOutlined style={{ color: "#6EC531", fontSize: "20px" }} />
-        ) : (
-          <CloseOutlined style={{ color: "#ED1C24", fontSize: "20px" }} />
-        )}
-      </p>
-    ),
-  },
-  {
-    align: "center",
-    title: "Acciones",
-    dataIndex: "id",
-    render: (id: number) => (
-      <Flex gap={4}>
-        <Link to={`/edit/${id}`}>
-          <Tooltip placement="top" title="Editar">
-            <Button type="primary" icon={<EditOutlined />} />
-          </Tooltip>
-        </Link>
-        <DeleteDialog
-          onDelete={() => {
-            handleDelete(id);
-          }}
-        />
-      </Flex>
-    ),
-  },
-];
-
 const StyledTable = styled(Table<MenuType>)`
   width: 100%;
 `;
 
 export const MenusTable = ({ data, onMenuSelected }: MenusTableProps) => {
+  const columns: TableColumnsType<MenuType> = [
+    {
+      title: "Imagen",
+      dataIndex: "imageUrl",
+      render: (imageUrl: string) => <StyledImage width={100} src={imageUrl} />,
+    },
+    {
+      title: "Nombre",
+      dataIndex: "name",
+    },
+    {
+      title: "Precio",
+      dataIndex: "price",
+      render: (price: number) => `$${price.toLocaleString("es-AR")}`,
+    },
+    {
+      align: "center",
+      title: "Activo",
+      dataIndex: "isDaysMenu",
+      render: (isDaysMenu: boolean) => (
+        <p>
+          {isDaysMenu ? (
+            <CheckOutlined style={{ color: "#6EC531", fontSize: "20px" }} />
+          ) : (
+            <CloseOutlined style={{ color: "#ED1C24", fontSize: "20px" }} />
+          )}
+        </p>
+      ),
+    },
+    {
+      align: "center",
+      title: "Acciones",
+      dataIndex: "id",
+      render: (id: number) => (
+        <Flex gap={4}>
+          <Link to={`/edit/${id}`}>
+            <Tooltip placement="top" title="Editar">
+              <Button type="primary" icon={<EditOutlined />} />
+            </Tooltip>
+          </Link>
+          <DeleteDialog
+            menu={data.find((menu) => menu.id === id!)!}
+            onDelete={() => {
+              handleDelete(id);
+            }}
+          />
+        </Flex>
+      ),
+    },
+  ];
+
   const rowSelection: TableProps<MenuType>["rowSelection"] = {
     onChange: (_, selectedRows: MenuType[]) => {
       onMenuSelected(selectedRows[0]);
