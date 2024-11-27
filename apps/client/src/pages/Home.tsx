9de21abae3d205daadd3260d38c1aa7d9a20bb8f
Nico Parra <no.gnzlz@gmail.com>
2024-11-27 02:10:40 -0300

    improve home using reducer

--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -1,55 +1,129 @@
 import { Button, Flex, notification } from "antd";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { MenusTable } from "../components/MenusTable";
 import { Card } from "../components/Card";
 import { MenuType } from "../interfaces";
 import { Link } from "react-router-dom";
 import api from "../service/api";
 
+type StateType = {
+  menus: MenuType[];
+  selectedMenu: MenuType | undefined;
+  loading: boolean;
+};
+
+enum ActionType {
+  FinishMenusFetch, // Finish fetching the menus
+  StartMenusUpdate, // Start updating the menus
+  FinishMenusUpdate, // Finish updating the menus
+  ChangeSelectedMenu, // Change the selected menu
+}
+
+type HomeAction = {
+  type: ActionType;
+  payload?: {
+    menus?: MenuType[];
+    updatedMenus?: MenuType[];
+    selectedMenu?: MenuType;
+  };
+};
+
+const HomeReducer = (state: StateType, action: HomeAction) => {
+  switch (action.type) {
+    case ActionType.FinishMenusFetch:
+      return {
+        ...state,
+        menus: action.payload?.menus || [],
+        loading: false,
+      };
+    case ActionType.StartMenusUpdate:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ActionType.FinishMenusUpdate:
+      return {
+        ...state,
+        menus: state.menus.map((menu) => {
+          const updatedMenu = action.payload?.updatedMenus?.find(
+            (updatedMenu) => updatedMenu.id === menu.id
+          );
+          return updatedMenu || menu;
+        }),
+        loading: false,
+      };
+    case ActionType.ChangeSelectedMenu:
+      return {
+        ...state,
+        selectedMenu: action.payload?.selectedMenu,
+      };
+    default:
+      return state;
+  }
+};
+
 function Home() {
-  const [menus, setMenus] = useState<MenuType[]>([]);
-  const [menu, setMenu] = useState<MenuType | undefined>();
-  const [loading, setLoading] = useState(false);
+  const [{ menus, selectedMenu, loading }, dispatch] = useReducer(HomeReducer, {
+    menus: [],
+    selectedMenu: undefined,
+    loading: true,
+  });
 
   const initData = async () => {
-    setMenus(
-      await api.fetchApi({
-        url: "/api/menu",
-        method: "GET",
-      })
-    );
+    const menus = await api.fetchApi<MenuType[]>({
+      url: "/api/menu",
+      method: "GET",
+    });
+    dispatch({
+      type: ActionType.FinishMenusFetch,
+      payload: { menus },
+    });
   };
 
   const clearPreviousMenu = async (menus: MenuType[]) => {
-    Promise.all(
-      menus.map((menu) =>
-        api.fetchApi({
+    dispatch({
+      type: ActionType.StartMenusUpdate,
+    });
+
+    const updatedMenus = await Promise.all(
+      menus.map((menu) => {
+        return api.fetchApi<MenuType>({
           url: `/api/menu/${menu.id}`,
           method: "PATCH",
           body: { isDaysMenu: false },
-        })
-      )
+        });
+      })
     );
+
+    dispatch({
+      type: ActionType.FinishMenusUpdate,
+      payload: { updatedMenus },
+    });
   };
 
   const handleChooseMenu = async () => {
-    if (menu) {
+    if (selectedMenu) {
       try {
-        setLoading(true);
         const prevMenus = await api.fetchApi<MenuType[]>({
           url: `/api/menu?isDaysMenu=true`,
           method: "GET",
         });
 
-        clearPreviousMenu(prevMenus);
+        await clearPreviousMenu(prevMenus);
 
-        await api.fetchApi({
-          url: `/api/menu/${menu.id}`,
+        dispatch({
+          type: ActionType.StartMenusUpdate,
+        });
+        const updatedMenu = await api.fetchApi<MenuType>({
+          url: `/api/menu/${selectedMenu.id}`,
           method: "PATCH",
           body: { isDaysMenu: true },
         });
 
-        setLoading(false);
+        dispatch({
+          type: ActionType.FinishMenusUpdate,
+          payload: { updatedMenus: [updatedMenu] },
+        });
 
         notification.success({
           message: "Menu del dia",
@@ -62,22 +136,12 @@ function Home() {
             "Ocurrio un error al intentar actualizar el menu del dia",
         });
       }
-
-      initData();
     }
   };
 
   useEffect(() => {
     initData();
   }, []);
-
-  useEffect(() => {
-    if (!menus.length) {
-      setLoading(true);
-    } else {
-      setLoading(false);
-    }
-  }, [menus]);
 
   return (
     <>
@@ -87,7 +151,7 @@ function Home() {
             <Link to="/add">
               <Button type="primary">Agregar menu</Button>
             </Link>
-            {menu && (
+            {selectedMenu && (
               <Button type="primary" onClick={handleChooseMenu}>
                 Elejir como menu del dia
               </Button>
@@ -96,7 +160,12 @@ function Home() {
           <Flex>
             <MenusTable
               data={menus}
-              onMenuSelected={setMenu}
+              onMenuSelected={(menu) =>
+                dispatch({
+                  type: ActionType.ChangeSelectedMenu,
+                  payload: { selectedMenu: menu },
+                })
+              }
               refetch={initData}
               loading={loading}
             />
